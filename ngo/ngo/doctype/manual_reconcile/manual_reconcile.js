5fa45626a6ef67539f7970f0db52d3f96bdf14fd
root <root@vmi337002.contaboserver.net>
2022-07-06 11:34:06 +0200

    working on manual reconcile

--- ngo/ngo/doctype/manual_reconcile/manual_reconcile.js
+++ ngo/ngo/doctype/manual_reconcile/manual_reconcile.js
@@ -2,7 +2,20 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Manual Reconcile', {
-	// refresh: function(frm) {
-
-	// }
+	get_data(frm){
+		frm.set_value("index", 0)
+		fram.refresh()
+	},
+	previous(frm){
+		let idx = frm.doc.index;
+		idx = idx -1;
+		if (idx<0){
+			frappe.throw("No Data")
+		}
+		let short_code = frm.doc.account[idx].account
+		frm.set_value("index", idx)
+		console.log(idx)
+		console.log(short_code)
+		frm.save()
+	}
 });
