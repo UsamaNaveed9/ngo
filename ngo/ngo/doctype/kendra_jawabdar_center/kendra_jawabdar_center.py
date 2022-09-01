ce555a2446414abdd6d7bb5ce103187e8a68f0b7
UsamaNaveed9 <usamanaveed9263@gmail.com>
2022-09-01 08:56:08 +0000

    fect: set naming series for all documents and made some changes

--- ngo/ngo/doctype/kendra_jawabdar_center/kendra_jawabdar_center.py
+++ ngo/ngo/doctype/kendra_jawabdar_center/kendra_jawabdar_center.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
 
-class KendraJawabdarCentre(Document):
+class KendraJawabdarCenter(Document):
 	pass
