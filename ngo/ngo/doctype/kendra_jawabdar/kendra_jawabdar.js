3e7afc6f2158f79a639729402f8c015ca63876b3
UsamaNaveed9 <usamanaveed9263@gmail.com>
2022-09-19 10:45:25 +0000

    fect:somes new changes

--- ngo/ngo/doctype/kendra_jawabdar/kendra_jawabdar.js
+++ ngo/ngo/doctype/kendra_jawabdar/kendra_jawabdar.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Kendra Jawabdar', {
-	// refresh: function(frm) {
-
-	// }
+	setup(frm) {
+		frm.fields_dict['list_of_authorized_centres'].grid.get_field("centres").get_query = function(doc, cdt, cdn) {
+	    return {
+		    filters: [
+			    ['Territory', 'is_group', '=','0'],
+		    ]
+	    }
+        }
+	}
 });
