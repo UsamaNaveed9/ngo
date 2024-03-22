ee15c3a0ecca52bd87cacac7be49fa2269fe8804
Milind <milindjadhao@Bizmap759.onmicrosoft.com>
2024-03-22 17:41:09 +0530

    Logic For CSV Extraction file

--- ngo/ngo/doctype/slip_form/slip_form.js
+++ ngo/ngo/doctype/slip_form/slip_form.js
@@ -2,25 +2,63 @@
 // For license information, please see license.txt
 frappe.ui.form.on('Slip Form', {
 	refresh:function(frm){
-		
 
-	}
+				
+
+	},
+	cheque_details_on_form_rendered: function(frm, cdt,cdn){
+
+
+	    
+	    // $('.previous').remove();
+        // $('.row-actions').append("<button class='btn btn-secondary btn-sm previous'>Previous</button> &nbsp;");
+        // $(".previous").off('click').on('click', function(){
+        //     cur_frm.open_grid_row().open_prev();
+        // });
+	    
+	    
+	    // // Check if the button already exists before appending it
+	    // $('.next').remove();
+        // $('.row-actions').append("<button class='btn btn-secondary btn-sm next'>Next</button> &nbsp;&nbsp;");
+        // $(".next").off('click').on('click', function(){
+        //     cur_frm.open_grid_row().open_next();
+        // });
+
+        
+        
+	
+
+	},
+
+
 
 });
 
 
-frappe.ui.form.on('Slip Cheque Form',{
-	refresh:function(frm){
-		
-	},
-    next:function(frm){
-        cur_frm.open_grid_row().open_next();
+frappe.ui.form.on('Slip Cheque Form', {
+    next: function(frm,cdt,cdn) {
+        var cheque_details = cur_frm.doc.cheque_details;
+        var child = locals[cdt][cdn];
+        var current_row_idx = child.idx
+        
+        if (current_row_idx < cheque_details.length) {
+            // Open the next row in the grid
+            cur_frm.open_grid_row().open_next();
+        } 
+        
+        else 
+			{ 
+
+				frappe.msgprint("All rows have been opened.");
+			
+			}
     },
-    previous:function(frm){
-        // $('[data-fieldname="cheque_details"]').find('[data-idx="3"] .btn-open-row').click()
+
+    previous: function(frm) {
         cur_frm.open_grid_row().open_prev();
     }
 });
+
 
 
 
--- ngo/ngo/doctype/slip_scanner_file_path/slip_scanner_file_path.py
+++ ngo/ngo/doctype/slip_scanner_file_path/slip_scanner_file_path.py
@@ -19,32 +19,36 @@ class SlipScannerFilePath(Document):
 
 def read_csv_(dict_list):
 		
-
 		filter_dict = []
 		slip_number_lst = []
 		missing_check_details = []
 
-		
 		for row in dict_list:
 			date_of_slip = row.get("A")  # Extract date_of_slip from dictionary
 			number = row.get("B")  # Extract number from dictionary
 			slip_number = None  # Initialize slip_number to None
 			pattern = r'\\([^\\$]+)\$'
 			number_ = re.findall(pattern, number)
+			
 			if number_:
 				slip_number = number_[0]
 				slip_number_lst.append(slip_number)
-			bank_details = row.get("C")
-			if isinstance(bank_details, str):
+			bank_details = row.get("C")	
+			
+
+
+			if isinstance(bank_details,str):
+				
 				bank_details = bank_details.replace('@', '')
 				bank_details_str = bank_details.split("<")
 				bank_details_str = [r for r in bank_details_str if r]
-				if len(bank_details_str) > 0 :
-
+				
+				if len(bank_details_str) > 0:
 					bank_str = " ".join(bank_details_str)
 					bank_str = bank_str.split(":")
 					bank_str = " ".join(bank_str)
 					bank_str = bank_str.split(" ")
+					
 
 					if len(bank_str) == 4:
 						# Extract check_number, micr_code, short_account_number, and branch_code
@@ -52,7 +56,6 @@ def read_csv_(dict_list):
 						micr_code = bank_str[1]
 						short_account_number = bank_str[2]
 						branch_code = bank_str[3]
-
 						# Append extracted information to filter_dict
 						filter_dict.append({
 							"date_of_slip": date_of_slip,
@@ -60,21 +63,52 @@ def read_csv_(dict_list):
 							"check_number": check_number,
 							"micr_code": micr_code,
 							"short_account_number": short_account_number,
-							"branch_code": branch_code
+							"branch_code": branch_code,
+							"check_path_number":number
+
 						})
+						
 					else:
+
+						filter_dict.append({
+							"date_of_slip": date_of_slip,
+							"slip_number": slip_number,
+							"missing_check_":number,
+							"check_path_number":number
+
+						})
+
 						missing_check_details.append(number)
-		
-		unique_account_number_lst = []
-			
+
+
+		df = pd.read_csv("/home/milind/Downloads/SVS_ScannedCheques267c79(1).csv",names=['A', 'B', 'C'])
+
+		check_details_list = df['B'].tolist()
+
+
+
+		list_of_check_number = []
 		for row in filter_dict:
-			row["Unique_number"] = row.get("micr_code") +  row.get("short_account_number")  +row.get("branch_code")
-			unique_account_number_lst.append(row.get("Unique_number"))
-
+			list_of_check_number.append(row.get("check_path_number"))
 
 		
 
+		for value in check_details_list:
+			if value not in list_of_check_number:
+				print(value,"=====")
 
+
+
+		unique_account_number_lst = []
+		for row in filter_dict:
+			micr_code = row.get("micr_code")
+			short_account_number = row.get("short_account_number")
+			branch_code = row.get("branch_code")
+			if micr_code is not None and short_account_number is not None and branch_code is not None:
+				row["Unique_number"] = micr_code + short_account_number + branch_code
+				unique_account_number_lst.append(row.get("Unique_number"))
+			
+		
 		bank_name_lst = []
 		bank_dict = []
 
@@ -133,7 +167,7 @@ def read_csv_(dict_list):
 				row["date_of_slip"] = formatted_date
 
 
-		return filter_dict,slip_number_lst
+		# return filter_dict,slip_number_lst
 
 
 				
@@ -143,7 +177,9 @@ def read_csv_(dict_list):
 def crete_entries_in_slip_form(filter_dict,slip_number_lst):
 	
 	existing_slip_number = []
+	
 	slip_number_lst_created_ = frappe.db.get_all("Slip Form",["slip_number"])
+	
 	for row in slip_number_lst_created_:
 		existing_slip_number.append(row.get("slip_number"))
 
@@ -192,9 +228,11 @@ def crete_entries_in_slip_form(filter_dict,slip_number_lst):
 						"clearing_status":"CREATED"
 
 					})
+					
 					slip.insert()
 					slip_number_ = row.get("slip_number")
 					slip_number_created.append(slip_number)			
+	
 	
 	return len(slip_number_created)
 
@@ -211,16 +249,50 @@ def crete_entries_in_slip_form(filter_dict,slip_number_lst):
 def read_csv(file):
 	
 	file_doc = frappe.get_doc("File",{"file_url":file})
-	file_path = frappe.utils.get_files_path(file_doc.file_name)
 	
-	df = pd.read_csv(file_path,names=['A', 'B', 'C'],nrows=200)
-	
+	file_url = file_doc.file_url
+
+	# file_path = frappe.utils.get_files_path(file_doc.file_name)
+
+	file = get_absolute_path(file_url)
+
+	df = pd.read_csv(file,names=['A', 'B', 'C'])
+
 	dict_list = df.to_dict('records')
+
+	# df = df.rename(columns={'A': 'Date_Time', 'B': 'check_details', 'C': 'bank_details'})
+	# df['Date_Time'] = pd.to_datetime(df['Date_Time'], format='%d-%m-%y %H:%M')
+	# df.insert(0, 'Date', df['Date_Time'].dt.date)
+	# df.drop(columns=['Date_Time'], inplace=True)
+	# pattern = r'\\([^\\$]+)\$'
+	# df['Slip_number'] = df['check_details'].str.extract(pattern)
+	# df['Bank_Details_Split'] = df['bank_details'].str.split('<')
+	# for row in dict_list:
+	# 	Bank_Details_Split = row.get("Bank_Details_Split")
+	# 	print()
+	# 	# Bank_Details_Split= [value  for value in Bank_Details_Split if value]
+	# 	Bank_Details_Split = ",".join(Bank_Details_Split)
+	# 	Bank_Details_Split = Bank_Details_Split.split(":")
+	# 	Bank_Details_Split = ",".join(Bank_Details_Split)
+	# 	Bank_Details_Split = Bank_Details_Split.split(",")
+	# 	row["Bank_Details_Split"] = Bank_Details_Split
 	
+	
+
+
 	filter_dict,slip_number_lst = read_csv_(dict_list)
 	
 	return crete_entries_in_slip_form(filter_dict, slip_number_lst)
-	
+
+
+def get_absolute_path(file_name):
+
+	file_path = ""
+	if(file_name.startswith('/files/')):
+		file_path += f'{frappe.utils.get_bench_path()}/sites/{frappe.utils.get_site_base_path()[2:]}/public{file_name}'
+	if(file_name.startswith('/private/')):
+		file_path += f'{frappe.utils.get_bench_path()}/sites/{frappe.utils.get_site_base_path()[2:]}{file_name}'
+	return file_path	
 	
 	
 	
