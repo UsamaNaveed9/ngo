8dfb7818afb1e6e0d5ea52282e37d81a58e00938
Neel Bhanushali <neal.bhanushali@gmail.com>
2024-07-05 18:38:24 +0530

    fix donor bank account with progress

--- ngo/patches/fix_donor_bank_accounts.py
+++ ngo/patches/fix_donor_bank_accounts.py
@@ -2,14 +2,18 @@ import frappe
 import csv
 
 def execute():
+  print("OPEN")
   csv_output = [["Donor", "Bank Account"]]
 
   frappe.db.delete("Donor Bank detail")
 
   donors = frappe.db.get_list("Donor")
+  print("DONOR COUNT", len(donors))
+  curr_donor = 1
 
   for i in donors:
     donor = frappe.get_doc("Donor", i.name)
+    print("CURR DONOR", curr_donor, i.name)
 
     bank_accounts = frappe.db.get_list("Bank Account", filters={
       "donor_id": donor.name
@@ -23,6 +27,7 @@ def execute():
       csv_output.append([donor.name, bank_account.name])
     
     donor.save()
+    curr_donor += 1
 
   with open(frappe.get_site_path() + "/Donor_Bank_Account_Fixed.csv", "w") as f:
     writer = csv.writer(f)
